dc3d3b0eb619f1accfdb0ff863c0459a0759578f
Timofey Hammer <timofey.hammer@gmail.com>
2023-07-10 21:04:46 +0300

    Fetch Dishes

--- YummieApp/Views/ListDishes/ListDishesViewController.swift
+++ YummieApp/Views/ListDishes/ListDishesViewController.swift
@@ -3,23 +3,32 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class ListDishesViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
 
     var category: DishCategory!
-    var dishes: [Dish] = [
-        .init(id: "id1", name: "Garri", description: "This is the best I have ever tasted", image: "https://picsum.photos /100/200", calories: 34),
-        .init(id: "id1", name: "Garri", description: "This is the best I have ever tasted", image: "https://picsum.photos /100/200", calories: 34),
-        .init(id: "id1", name: "Garri", description: "This is the best I have ever tasted", image: "https://picsum.photos /100/200", calories: 34)
-    ]
+    var dishes: [Dish] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = category.name
         registerCells()
+        
+        ProgressHUD.show()
+        NetworkService.shared.fetchCargeorysDishes(categoryId: category.id ?? "") { [weak self] (result) in
+            switch result {
+            case .success(let dishes):
+                ProgressHUD.dismiss()
+                self?.dishes = dishes
+                self?.tableView.reloadData()
+            case .failure(let error):
+                ProgressHUD.showError(error.localizedDescription)
+            }
+        }
     }
     
     private func registerCells() {
